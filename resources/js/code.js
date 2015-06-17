827a4200c09d079c25affeebb9ebc0b5fb383c04
Klaus Borges <klausborgs@gmail.com>
2015-06-17 06:51:48 -0300

    Last bit of clean-up before handing out the assignment

--- resources/js/code.js
+++ resources/js/code.js
@@ -2,16 +2,7 @@ $(document).ready(function() {
   // Switches code shown in panels
   $('#pill-c').click(function() {
     $('#pill-c').attr('class', 'active');
-    $('#pill-go').attr('class', '');
 
-    $('.code-go').hide();
     $('.code-c').show();
-  });
-  $('#pill-go').click(function() {
-    $('#pill-c').attr('class', '');
-    $('#pill-go').attr('class', 'active');
-
-    $('.code-c').hide();
-    $('.code-go').show();
   });
 });
